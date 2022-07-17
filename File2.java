f62fc5252598ef2d637cef9e4af1b096b3592f3c
nipuni-sathsarani <79597637+nipuni-sathsarani@users.noreply.github.com>
2022-07-17 13:43:44 +0530

    File2

---
+++ File2.java
@@ -0,0 +1,13 @@
+package FP_Experiments;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class File2 {
+	public static void main(String[] args)throws Exception{
+		BufferedReader r = new BufferedReader(new FileReader("C:/Users/ACER/eclipse-workspace/repository6/repository6/JavaFP/data/Vehicles.txt"));
+		//r.lines().forEach(l->System.out.println(l));
+		System.out.println(r.lines().count());
+		r.close();
+	}
+}
