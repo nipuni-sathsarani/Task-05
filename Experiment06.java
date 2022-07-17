3418bada508f74849f8296c49af8e6abe657aba5
nipuni-sathsarani <79597637+nipuni-sathsarani@users.noreply.github.com>
2022-07-17 13:35:12 +0530

    Experiment06

---
+++ Experiment06.java
@@ -0,0 +1,29 @@
+package FP_Experiments;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Experiment06 {
+	
+	class StringPrintConsumer implements Consumer<String>{
+		public void accept(String str) {
+			System.out.println(str);
+		}
+	}
+	
+	public void run() {
+	  String[] n1 = {"Toyota", "BMW", "Honda","Suzuki","Volvo"};
+		
+		List<String> n2 = Arrays.asList(n1);
+		
+		n2.forEach(new StringPrintConsumer());
+	}
+	
+	public static void main(String[] args) {
+		new Experiment06().run();
+	}
+
+}
+
+
