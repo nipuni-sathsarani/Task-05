6ebf1e5064bbf3f49144f30427bb3fbbb36d28e8
nipuni-sathsarani <79597637+nipuni-sathsarani@users.noreply.github.com>
2022-07-17 13:30:25 +0530

    Experiment01

---
+++ Experiment01.java
@@ -0,0 +1,17 @@
+package FP_Experiments;
+
+public class Experiment01 {
+	
+public static void main(String[] args) {
+	String[] n1 = {"Toyota", "BMW", "Honda","Suzuki","Volvo"};
+	
+	System.out.println(n1.getClass());
+	
+	for(int i=0;i<n1.length;i++) {
+		System.out.println(n1[i]);
+	}
+
+}
+}
+
+
